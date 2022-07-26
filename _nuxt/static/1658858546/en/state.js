eed8fff871025789a63ae5f855d304ef9d7897d2
ts-nextevolution <ts-nextevolution@users.noreply.github.com>
2022-07-26 18:02:27 +0000

    deploy: 7b2a3f18905b536173698a0785366bd9c3e86b9f

---
+++ _nuxt/static/1658858546/en/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdeepops-website\u002F_nuxt\u002Fstatic\u002F1658858546",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen",config:{_app:{basePath:"\u002Fdeepops-website\u002F",assetsPath:"\u002Fdeepops-website\u002F_nuxt\u002F",cdnURL:a}}}}(null));
