4b71d5079119916f2b6892d806ec6b51b7cdde9e
Sander, Till <till.sander@nextevolution.de>
2022-07-27 10:11:36 +0200

    Added github pages disclaimer to the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="">
+  <div class="max-w-screen-2xl px-4 2xl:px-0 py-0 xl:py-4 mx-auto">
     <!--Footer-->
     <div
-      class="flex flex-row max-w-screen-2xl px-4 2xl:px-0
-     mx-auto items-center justify-between py-0 xl:py-4"
+      class="flex flex-row items-center justify-between"
     >
-      <div class="p-4 md:p-16 w-auto">
+      <div class="px-4 md:px-16 w-auto">
         <a class="text-gray-500 no-underline hover:no-underline" href="#">© Deepshore GmbH 2022</a>
       </div>
-      <div class="p-4 md:p-16 w-auto">
+      <div class="px-4 md:px-16 w-auto">
         <a class="text-gray-500 no-underline hover:no-underline" href="https://deepshore.de/company/contact-imprint-privacy">Impressum/Imprint</a>
       </div>
+    </div>
+    <!-- Text -->
+    <div>
+      <p class="text-gray-400 text-sm px-4 md:px-16 py-4">
+        GitHub Pages <br/>
+        Wenn Sie unsere GitHub Pages besuchen oder nutzen, werden von uns keinerlei personenbezogene Daten erhoben. Es findet keine Analyse statt, es sind keine externen Quellen eingebunden, und es werden keine Cookies von uns gesetzt.
+        GitHub Pages werden auf Servern der Github Inc., 88 Colin P Kelly Jr. Street, San Francisco, CA 94107, USA gehostet. GitHub kann beim Aufruf der Seiten technische Informationen und auch die IP-Adresse erfassen. Hierfür und eventuelle weitere Verarbeitungen ist GitHub datenschutzrechtlich verantwortlich. Informationen zum Umgang mit Nutzerdaten können in der Datenschutzerklärung von GitHub unter <a href="https://help.github.com/articles/github-privacy-policy">help.github.com/articles/github-privacy-policy</a> abgerufen werden.
+      </p>
     </div>
   </div>
 </template>
