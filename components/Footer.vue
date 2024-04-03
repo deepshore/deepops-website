21eda535d287be925655071d49c35c4a86b0e104
Selamanse <selamanse@scheinfrei.info>
2024-04-03 21:22:13 +0200

    Adjust footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -11,13 +11,5 @@
         <a class="text-gray-500 no-underline hover:no-underline" href="https://deepshore.de/company/contact-imprint-privacy">Impressum/Imprint</a>
       </div>
     </div>
-    <!-- Text -->
-    <div>
-      <p class="text-gray-400 text-sm px-4 md:px-16 py-4">
-        GitHub Pages <br>
-        Wenn Sie unsere GitHub Pages besuchen oder nutzen, werden von uns keinerlei personenbezogene Daten erhoben. Es findet keine Analyse statt, es sind keine externen Quellen eingebunden, und es werden keine Cookies von uns gesetzt.
-        GitHub Pages werden auf Servern der Github Inc., 88 Colin P Kelly Jr. Street, San Francisco, CA 94107, USA gehostet. GitHub kann beim Aufruf der Seiten technische Informationen und auch die IP-Adresse erfassen. Hierfür und eventuelle weitere Verarbeitungen ist GitHub datenschutzrechtlich verantwortlich. Informationen zum Umgang mit Nutzerdaten können in der Datenschutzerklärung von GitHub unter <a href="https://help.github.com/articles/github-privacy-policy">help.github.com/articles/github-privacy-policy</a> abgerufen werden.
-      </p>
-    </div>
   </div>
 </template>
